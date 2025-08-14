d9af7230a99a10a622dfe69d5a5f13ece195b3e1
laalucas-us <laalucas88@gmail.com>
2025-08-15 00:16:23 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,11 +2,11 @@
 
 # 配置参数
 export uuid="6e9a8760-6ced-4bc2-99dc-e0be8e71147d"
-export vmpt="54321"
+export vmpt=""
+export hypt=""
 export argo="y"
-export agn="senya.sigee.dpdns.org"
-export agk="eyJhIjoiM2E5ODM4YmE1Y2I1ZjA1ODFkYjQ5YzZjNGI0Y2E5ODYiLCJ0IjoiMGIxYjM5YjEtYjhhNC00ZWUwLWFlZTUtNTMxYzllNWZkNTQ3IiwicyI6Ik9UZzNOMlV3T0dJdFkyWmxaQzAwT0RKaUxXSTRPV1F0TXpRd01qUmxOV1k1WVdReCJ9"
-export PORT=8443
+export agn=""
+export agk=""
 
 # 检查端口是否占用
 if lsof -i TCP:${PORT} -sTCP:LISTEN >/dev/null 2>&1; then
